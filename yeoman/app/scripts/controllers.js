4c2b188204a0229621e669b932b7cb913ba8d2a7
pascal leclercq <pascal.leclercq@gmail.com>
2013-10-04 22:16:49 +0200

    integrate imdb search

---
+++ yeoman/app/scripts/controllers.js
@@ -0,0 +1,22 @@
+angular.module("yeomanApp", [ 'ngResource' ]);
+
+function MovieListCtrl($scope, $resource, $http) {
+
+	$scope.search = function(movieToSearch) {
+
+		$http({
+			method : 'jsonp',
+			url : 'http://www.omdbapi.com/',
+			params : {
+				s : movieToSearch,
+				callback : 'JSON_CALLBACK'
+			}
+		}).success(function(data, status, headers, config) {
+			$scope.movies = data.Search;
+		}).error(function(data, status, headers, config) {
+			// called asynchronously if an error occurs
+			// or server returns response with an error status.
+			alert(status);
+		});
+	}
+}
