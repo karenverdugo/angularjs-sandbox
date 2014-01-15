a1d1ee30fd73a9e8083658f14bfb69ae7c81736b
pascalleclercq <pascal.leclercq@gmail.com>
2014-01-16 00:04:25 +0100

    fix deps fix loop with ng-view..

--- yeoman/app/scripts/app.js
+++ yeoman/app/scripts/app.js
@@ -2,10 +2,7 @@
 
 angular.module('yeomanApp', [ 'ngResource', 'ngRoute' ]).config(
 		function($routeProvider) {
-			$routeProvider.when('/', {
-				templateUrl : 'index.html',
-				controller : 'MovieListCtrl'
-			}).when('/movie/:movieId', {
+			$routeProvider.when('/movie/:movieId', {
 				templateUrl : 'views/movie.html',
 				controller : 'MovieController'
 			}).otherwise({
